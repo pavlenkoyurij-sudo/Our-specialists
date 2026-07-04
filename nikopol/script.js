f724234dddf99e7756064e34075cede81d63530d
pavlenkoyurij-sudo <pavlenkoyurij@gmail.com>
2026-07-04 21:19:20 +0300

    Update script.js

--- nikopol/script.js
+++ nikopol/script.js
@@ -207,7 +207,7 @@
         btn.addEventListener("click", () => {
             window.scrollTo({
                 top: 0,
-                behavior: "smooth"//забезпечує плавний скролінг
+                behavior: "smooth"//забезпечує плавний скрол
             });
         });
 
