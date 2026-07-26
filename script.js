d437261b416b86eaf8fc698479690f6bae232e95
pavlenkoyurij-sudo <pavlenkoyurij@gmail.com>
2026-07-27 00:00:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -203,7 +203,7 @@
 
             {
                 id: 14,
-                isPremium: false,
+                isPremium: true,
                 name: "Шукаємо майстрів",
                 photo: "images/noCard.jpeg",
                 profession: "Ми чекаємо саме вас",
@@ -213,8 +213,8 @@
                 experience:"",
                 city: "Покров",
                 phone: "380957996024",
-                description: "Зателефонуйте нам або напишіть",
-                page:""
+                description: "Напишіть нам у WhatsApp",
+                page:"https://wa.me/380957996024?text=Добрий%20день!%20Хочу%20приєднатися%20до%20каталогу%20майстрів%20Покрова."
             },
         ];
 
